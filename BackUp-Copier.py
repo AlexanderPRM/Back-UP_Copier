8796604856b3bfe728e7ccd47fe83997cfaa972d
Александр Пермяков <nathan920@yandex.ru>
2022-07-02 19:25:56 +0300

    Version 1.9.1

--- BackUp-Copier.py
+++ BackUp-Copier.py
@@ -2,6 +2,7 @@ import requests
 from pprint import pprint
 from time import sleep
 from alive_progress import alive_bar
+from system_files import more_option, write
 
 class CopyPhotoToYandex:
     """Класс для резервного копирования фото с соц сети на облако"""
@@ -115,48 +116,10 @@ class CopyPhotoToYandex:
               bar()
         print(mark * 30)
 
-def more_option():
-    mark = '-'
-    params = []
-    question = input('Хотите указать дополнительные параметры? ДА\НЕТ: ')
-    print(mark * 30)
-    while question.strip() != 'ДА' or question.strip() != 'НЕТ':
-        if question.upper().strip() == 'ДА':
-            count_photos = input('''\nКакое количество фото хотите загрузить?
-            Можете нажать ENTER и будет выставленно значение - 5: ''')
-            if count_photos == '':
-                count_photos = 5
-            params.append(count_photos)
-            dir = input('Как назвать папку на вашем диске?: ')
-            params.append(dir)
-            album = input('Что будем загружать, аватарки или фото со стены? АВЫ\СТЕНА: ')
-            if album.lower().strip() == 'авы':
-                params.append('profile')
-            elif album.lower().strip() == 'стена':
-                params.append('wall')
-            return params
-        elif question.upper().strip() == 'НЕТ':
-            count_photos = 5
-            params.append(count_photos)
-            dir = 'BackUp-Program'
-            params.append(dir)
-            album = 'profile'
-            params.append(album)
-            print(f'''Выставлены значения по умолчания.
-            Количество фото - {count_photos}
-            Наименование папки - {dir}
-            Альбом загрузки - {album}''')
-            return params
-        else:
-            print('Разработчик не реализовал другие варианты, так что следуйте инструкции :)')
-            question = input('Хотите указать дополнительные параметры? ДА\НЕТ: ')
-
-
-
 if __name__ == '__main__':
     mark = '-'
     # Hello Words
-    print(f'\nПРИВЕТСТВУЮ, ЭТО РЕЗЕРВНЫЙ КОПИРОВАЛЬЩИК.\n{mark * 30}')
+    write(f'\nПРИВЕТСТВУЮ, ЭТО РЕЗЕРВНЫЙ КОПИРОВАЛЬЩИК.\n{mark * 30}\n')
     # vk =
     # yandex_ =
     id_vk = int(input('Введите пожалуйста ID аккаунта VK: '))
@@ -171,7 +134,7 @@ if __name__ == '__main__':
     Me.upload_photos()
     print('Ваши фото:\n')
     pprint(Me.name_photo(), width=1, indent=2)
-    print(f'{mark * 30}\nПрограмма сработала успешно, проверьте диск.')
+    write(f'{mark * 30}\nПрограмма сработала успешно, проверьте диск.\n')
     input('Введите ENTER для выхода: ')
 
 
