2dcbc18176432308500fe7f3518b906304d0f744
Александр Пермяков <nathan920@yandex.ru>
2022-07-07 18:10:29 +0300

    Network Classes

---
+++ NetworkClasses.py
@@ -0,0 +1,80 @@
+import requests
+from pprint import pprint
+
+class VK:
+    url = 'https://api.vk.com/method/'
+    token = ''''''
+
+    def __init__(self, id, count_photos, album):
+        self.params = {'access_token': self.token,
+                       'owner_id': id,
+                       'v': 5.131,
+                       }
+        self.count_photos = count_photos
+        self.album = album
+
+    # Получение фото с профиля
+    def get_photos(self):
+        get_photos_url = self.url + 'photos.get'
+        get_photos_params = {'album_id': self.album,
+                             'extended': 1,
+                             'count': self.count_photos}
+        response = requests.get(get_photos_url, params={**self.params, **get_photos_params}).json()
+        return response
+
+    # Отсеивание ссылок и размера
+    def url_photos(self):
+        photos = self.get_photos()['response']['items']
+        info_all_photos = []
+        for post in photos:
+            list_info_photos = []
+            max_height = 0
+            max_width = 0
+            # pprint(post)
+            sizes_photo = post['sizes']
+            # pprint(len(sizes_photo))
+            counter = 0
+            flag = True
+            for size in sizes_photo:
+                if size['height'] >= max_height and size['width'] >= max_width:
+                    max_height = size['height']
+                    max_width = size['width']
+            for size in sizes_photo:
+                counter += 1
+                if (size['height'] == max_height and size['width'] == max_width) and size['height'] != 0:
+                    list_info_photos.append(size['url'])
+                    list_info_photos.append(size['type'])
+                    flag = False
+                    break
+                elif flag and counter == len(sizes_photo):
+                    list_info_photos.append(sizes_photo[-1]['url'])
+                    list_info_photos.append(sizes_photo[-1]['type'])
+
+            info_all_photos.append(list_info_photos)
+        return info_all_photos
+
+    # Наименование фото и размер фото
+    def name_photo(self):
+        photos = self.get_photos()['response']['items']
+        list_photos = []
+        count = -1
+
+        for photo in photos:
+
+            photo_info = {}
+            count += 1
+            flag = True
+            photo_name = str(photo['likes']['count']) + '.jpg'
+            if count != 0:
+                for file_info in list_photos:
+                    if file_info['filename'] == photo_name:
+                        photo_info['filename'] = photo_name + f' \ndate - {photo["date"]}'
+                        flag = False
+                    if flag:
+                        photo_info['filename'] = photo_name
+            else:
+                photo_info['filename'] = photo_name
+            photo_info['size'] = self.url_photos()[count][1]
+            list_photos.append(photo_info)
+        return list_photos
+
