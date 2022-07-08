57465be68c4de52de53efd650e0a726941e3494a
Александр Пермяков <nathan920@yandex.ru>
2022-07-08 22:51:46 +0300

    Version 2.9

--- BackUp-Copier.py
+++ BackUp-Copier.py
@@ -1,34 +1,33 @@
 
 from pprint import pprint
-from system_files import more_option, write
+from system_files import more_option, write, check_tokens
 from NetworkClasses import VK
 from DiskClasses import YandexDisk
 import json
+import requests
+
 
 
 if __name__ == '__main__':
     mark = '-'
-    # Hello Words
     write(f'\nПРИВЕТСТВУЮ, ЭТО РЕЗЕРВНЫЙ КОПИРОВАЛЬЩИК.\n{mark * 30}\n')
-    # vk =
-    yandex_ = ''
-#     id_vk = int(input('Введите пожалуйста ID аккаунта VK: '))
-#     yandex = input('''\nТеперь вам нужно будет ввести токен своего YANDEX-DISK,
-# Это нужно для того чтобы мы знали куда вам отправить файл. Узнать свой токен можно тут - https://yandex.ru/dev/disk/poligon/
-# Ввод: ''')
-#     #user_params = [7, 'тесты', 'profile']
+
+    # Функции в файле system_files.py
+    main_info = check_tokens()
     user_params = more_option()
-    Me_VK = VK(397101429, user_params[0], user_params[2])
-    Me = YandexDisk(yandex_token=yandex_, dir=user_params[1])
+
+    # Создание классов
+    Me_VK = VK(main_info[0], user_params[0], user_params[2])
+    Me_Yandex = YandexDisk(yandex_token=main_info[1], dir=user_params[1])
+
+    # Работа программы
     print(f'{mark * 30}\nЗагрузка фото...\n{mark * 30}')
-    Me.upload_photos(Me_VK)
+    Me_Yandex.upload_photos(Me_VK)
     print('Ваши фото:\n')
     name_photos = Me_VK.name_photo()
     pprint(name_photos, width=1, indent=2)
-    with open('name_files.json', 'w') as writer:
+    with open('info_files.json', 'w') as writer:
         json.dump(name_photos, writer)
+        print('\nВаши фото записаны в файл - info_files.json')
     write(f'{mark * 30}\nПрограмма сработала успешно, проверьте диск.\n')
     input('Введите ENTER для выхода: ')
-
-
-
